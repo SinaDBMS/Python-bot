13d16f9730c05e85cb18d81ec1817cb03e18b7cf
Sina BD <sina_bd@outlook.com>
2017-08-29 13:25:23 +0430

    Thread testing

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from flask import Flask
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.filters import Filters
@@ -6,8 +8,12 @@ from telegram.ext.updater import Updater
 
 import costume_handlers
 from costume_handlers import photo_handler, video_handler
+from post_scheduling import threaded_function
 
 application = Flask(__name__)
+
+thread = Thread(target=threaded_function)
+thread.start()
 
 
 @application.route("/")
