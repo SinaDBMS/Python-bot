6c2e30b5c07425592ed38ba38e29c62c036aa039
Sina BD <sina_bd@outlook.com>
2017-08-29 13:19:56 +0430

    Telegram bot removed

--- wsgi.py
+++ wsgi.py
@@ -15,16 +15,5 @@ def hello():
     return "Hello World!"
 
 
-updater = Updater('413427401:AAEgcTahApxJLAPGHK43TfJAl40K7CdJ8pw')
-dispatcher = updater.dispatcher
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-photo = MessageHandler(Filters.photo, photo_handler)
-video = MessageHandler(Filters.video, video_handler)
-view_q = CommandHandler('viewqueue', costume_handlers.view_queue)
-dispatcher.add_handler(photo)
-dispatcher.add_handler(video)
-dispatcher.add_handler(view_q)
-updater.start_polling()
-
 if __name__ == "__main__":
     application.run()
