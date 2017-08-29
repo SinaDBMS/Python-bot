b183b198af79e9e4999e7ceaa93b5bc1da675ef8
Sina BD <sina_bd@outlook.com>
2017-08-29 13:16:55 +0430

    Telegram bot started

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.filters import Filters
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.updater import Updater
+
+import costume_handlers
+from costume_handlers import photo_handler, video_handler
+
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+updater = Updater('413427401:AAEgcTahApxJLAPGHK43TfJAl40K7CdJ8pw')
+dispatcher = updater.dispatcher
+# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+photo = MessageHandler(Filters.photo, photo_handler)
+video = MessageHandler(Filters.video, video_handler)
+view_q = CommandHandler('viewqueue', costume_handlers.view_queue)
+dispatcher.add_handler(photo)
+dispatcher.add_handler(video)
+dispatcher.add_handler(view_q)
+updater.start_polling()
 
 if __name__ == "__main__":
     application.run()
